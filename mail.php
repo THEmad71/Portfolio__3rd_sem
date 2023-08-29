bdbd1bf898caf0a5382552cd8410fb88f1ec5798
THEmad71 <mrbangladesh19521971@gmail.com>
2023-08-30 02:20:09 +0600

    8/30/2023

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$number= $_POST['mobile'];
+
+
+
+$to = "mrbangladesh19521971@gmail.com";
+$subject = "Mail From Amad uddin osama";
+$txt ="Name = ". $name . "\r\n  
+Email = " . $email . "\r\n 
+Message =" . $message;\r\n 
+Mobile Number = ". $number; 
+
+$headers = "From: noreply@amadzone.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
